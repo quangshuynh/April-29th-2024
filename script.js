beece8a5fe12e7f1f50e1fe9067405d9eb0f9d42
qth9368 <qth9368@rit.edu>
2024-04-29 11:48:09 +0000

    music button, more fonts

--- script.js
+++ script.js
@@ -6,3 +6,40 @@
           element.style.display = 'block';
       }
   }
+
+function personalizedGreeting() {
+    const today = new Date();
+    const hour = today.getHours();
+    let greeting;
+
+    if(hour < 12) {
+        greeting = "Good morning, beautiful!";
+    } else if(hour < 18) {
+        greeting = "Good afternoon, my love!";
+    } else {
+        greeting = "Good evening, darling!";
+    }
+
+    document.getElementById("personalGreeting").innerText = greeting;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    personalizedGreeting();
+});
+
+function toggleMusic() {
+    var music = document.getElementById('bgMusic');
+    if(music.paused) {
+        music.play();
+    } else {
+        music.pause();
+    }
+}
+
+function setVolume(value) {
+    var music = document.getElementById('bgMusic');
+    music.volume = value;
+}
+
+
+
